09989a3781da28a64913ffe296d1b800697fc45a
Afrina Noor <tasmimafrin1189@gmail.com>
2025-11-17 01:21:45 +0600

    Update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -67,10 +67,32 @@ const Hero = () => {
       colors: ["#c94f4f", "#9D0808"],
       main: cherryIcecream,
       fruits: [
-        { src: cherry1, style: { top: "8%", left: "15%", width: "260px", rotate: "-40deg" } },
-        { src: cherry2, style: { top: "30%", right: "15%", width: "260px", rotate: "15deg" } },
-        { src: cherry3, style: { bottom: "10%", left: "20%", width: "260px", rotate: "-20deg" } },
-        { src: cherry4, style: { bottom: "-5%", right: "25%", width: "260px", rotate: "10deg" } },
+        {
+          src: cherry1,
+          style: { top: "8%", left: "15%", width: "260px", rotate: "-40deg" },
+        },
+        {
+          src: cherry2,
+          style: { top: "30%", right: "15%", width: "260px", rotate: "15deg" },
+        },
+        {
+          src: cherry3,
+          style: {
+            bottom: "10%",
+            left: "20%",
+            width: "260px",
+            rotate: "100deg",
+          },
+        },
+        {
+          src: cherry4,
+          style: {
+            bottom: "-5%",
+            right: "25%",
+            width: "260px",
+            rotate: "10deg",
+          },
+        },
       ],
     },
     {
@@ -80,10 +102,32 @@ const Hero = () => {
       colors: ["#B5D37A", "#9CB246"],
       main: appleIcecream,
       fruits: [
-        { src: apple1, style: { top: "5%", left: "10%", width: "260px", rotate: "-5deg" } },
-        { src: apple2, style: { top: "20%", right: "12%", width: "260px", rotate: "10deg" } },
-        { src: apple3, style: { bottom: "2%", left: "22%", width: "260px", rotate: "-12deg" } },
-        { src: apple4, style: { bottom: "10%", right: "25%", width: "260px", rotate: "8deg" } },
+        {
+          src: apple1,
+          style: { top: "5%", left: "10%", width: "260px", rotate: "-5deg" },
+        },
+        {
+          src: apple2,
+          style: { top: "20%", right: "12%", width: "260px", rotate: "10deg" },
+        },
+        {
+          src: apple3,
+          style: {
+            bottom: "2%",
+            left: "22%",
+            width: "260px",
+            rotate: "-12deg",
+          },
+        },
+        {
+          src: apple4,
+          style: {
+            bottom: "10%",
+            right: "25%",
+            width: "260px",
+            rotate: "8deg",
+          },
+        },
       ],
     },
     {
@@ -92,11 +136,34 @@ const Hero = () => {
       desc: "Dive into rich, velvety chocolate ice cream that satisfies your cravings with deep cocoa goodness in every bite.",
       colors: ["#996F5B", "#722D1D"],
       main: chocolateIcecream,
+      mainStyle: { width: "560px" },
       fruits: [
-        { src: choco1, style: { top: "10%", left: "19%", width: "260px", rotate: "-10deg" } },
-        { src: choco2, style: { top: "25%", right: "15%", width: "260px", rotate: "5deg" } },
-        { src: choco3, style: { bottom: "10%", left: "25%", width: "260px", rotate: "-15deg" } },
-        { src: choco4, style: { bottom: "12%", right: "20%", width: "260px", rotate: "10deg" } },
+        {
+          src: choco1,
+          style: { top: "10%", left: "19%", width: "260px", rotate: "-10deg" },
+        },
+        {
+          src: choco2,
+          style: { top: "25%", right: "15%", width: "260px", rotate: "5deg" },
+        },
+        {
+          src: choco3,
+          style: {
+            bottom: "10%",
+            left: "25%",
+            width: "260px",
+            rotate: "-15deg",
+          },
+        },
+        {
+          src: choco4,
+          style: {
+            bottom: "12%",
+            right: "20%",
+            width: "260px",
+            rotate: "10deg",
+          },
+        },
       ],
     },
     {
@@ -106,10 +173,32 @@ const Hero = () => {
       colors: ["#ffdeb3", "#DB893F"],
       main: peachIcecream,
       fruits: [
-        { src: peach1, style: { top: "8%", left: "8%", width: "360px", rotate: "-8deg" } },
-        { src: peach2, style: { top: "28%", right: "9%", width: "360px", rotate: "10deg" } },
-        { src: peach3, style: { bottom: "1%", left: "20%", width: "360px", rotate: "-12deg" } },
-        { src: peach4, style: { bottom: "-5%", right: "22%", width: "360px", rotate: "8deg" } },
+        {
+          src: peach1,
+          style: { top: "8%", left: "8%", width: "360px", rotate: "-8deg" },
+        },
+        {
+          src: peach2,
+          style: { top: "28%", right: "9%", width: "360px", rotate: "10deg" },
+        },
+        {
+          src: peach3,
+          style: {
+            bottom: "1%",
+            left: "20%",
+            width: "360px",
+            rotate: "-12deg",
+          },
+        },
+        {
+          src: peach4,
+          style: {
+            bottom: "-5%",
+            right: "22%",
+            width: "360px",
+            rotate: "8deg",
+          },
+        },
       ],
     },
     {
@@ -119,10 +208,32 @@ const Hero = () => {
       colors: ["#b26cf2", "#9b4fe0"],
       main: blueberryIcecream,
       fruits: [
-        { src: blueberry1, style: { top: "10%", left: "12%", width: "260px", rotate: "-5deg" } },
-        { src: blueberry2, style: { top: "25%", right: "15%", width: "260px", rotate: "80deg" } },
-        { src: blueberry3, style: { bottom: "8%", left: "25%", width: "260px", rotate: "-10deg" } },
-        { src: blueberry4, style: { bottom: "10%", right: "20%", width: "260px", rotate: "6deg" } },
+        {
+          src: blueberry1,
+          style: { top: "10%", left: "12%", width: "260px", rotate: "-5deg" },
+        },
+        {
+          src: blueberry2,
+          style: { top: "25%", right: "15%", width: "260px", rotate: "80deg" },
+        },
+        {
+          src: blueberry3,
+          style: {
+            bottom: "8%",
+            left: "25%",
+            width: "260px",
+            rotate: "-10deg",
+          },
+        },
+        {
+          src: blueberry4,
+          style: {
+            bottom: "10%",
+            right: "20%",
+            width: "260px",
+            rotate: "6deg",
+          },
+        },
       ],
     },
     {
@@ -131,11 +242,34 @@ const Hero = () => {
       desc: "Classic strawberry sweetness blended into creamy scoops that are vibrant, fruity, and irresistible.",
       colors: ["#ff9ebc", "#d7265f"],
       main: strawberryIcecream,
+      mainStyle: { width: "540px" },
       fruits: [
-        { src: strawberry1, style: { top: "8%", left: "15%", width: "260px", rotate: "-10deg" } },
-        { src: strawberry2, style: { top: "28%", right: "18%", width: "260px", rotate: "12deg" } },
-        { src: strawberry3, style: { bottom: "14%", left: "22%", width: "200px", rotate: "-8deg" } },
-        { src: strawberry4, style: { bottom: "12%", right: "15%", width: "260px", rotate: "10deg" } },
+        {
+          src: strawberry1,
+          style: { top: "8%", left: "15%", width: "260px", rotate: "-10deg" },
+        },
+        {
+          src: strawberry2,
+          style: { top: "20%", right: "18%", width: "260px", rotate: "12deg" },
+        },
+        {
+          src: strawberry3,
+          style: {
+            bottom: "14%",
+            left: "22%",
+            width: "200px",
+            rotate: "-8deg",
+          },
+        },
+        {
+          src: strawberry4,
+          style: {
+            bottom: "12%",
+            right: "15%",
+            width: "260px",
+            rotate: "10deg",
+          },
+        },
       ],
     },
     {
@@ -144,11 +278,34 @@ const Hero = () => {
       desc: "Nutty pistachio ice cream that’s rich, creamy, and perfect for a sophisticated, crunchy-smooth flavor experience.",
       colors: ["#d5e5c0", "#a3b97f"],
       main: pistachioIcecream,
+      mainStyle: { width: "670px" },
       fruits: [
-        { src: pistachio1, style: { top: "10%", left: "12%", width: "260px", rotate: "-10deg" } },
-        { src: pistachio2, style: { top: "25%", right: "15%", width: "260px", rotate: "10deg" } },
-        { src: pistachio3, style: { bottom: "12%", left: "20%", width: "260px", rotate: "-8deg" } },
-        { src: pistachio4, style: { bottom: "10%", right: "18%", width: "260px", rotate: "6deg" } },
+        {
+          src: pistachio1,
+          style: { top: "10%", left: "25%", width: "120px", rotate: "-200deg" },
+        },
+        {
+          src: pistachio2,
+          style: { top: "25%", right: "15%", width: "160px", rotate: "10deg" },
+        },
+        {
+          src: pistachio3,
+          style: {
+            bottom: "12%",
+            left: "25%",
+            width: "120px",
+            rotate: "-8deg",
+          },
+        },
+        {
+          src: pistachio4,
+          style: {
+            bottom: "10%",
+            right: "20%",
+            width: "120px",
+            rotate: "6deg",
+          },
+        },
       ],
     },
     {
@@ -157,24 +314,70 @@ const Hero = () => {
       desc: "Smooth, rich hazelnut ice cream that melts effortlessly in your mouth, delivering nutty satisfaction in every bite.",
       colors: ["#d9c3a1", "#a67c52"],
       main: hazelnutIcecream,
+      mainStyle: { width: "430px" },
       fruits: [
-        { src: hazelnut1, style: { top: "10%", left: "15%", width: "260px", rotate: "-10deg" } },
-        { src: hazelnut2, style: { top: "25%", right: "15%", width: "260px", rotate: "10deg" } },
-        { src: hazelnut3, style: { bottom: "12%", left: "18%", width: "260px", rotate: "-8deg" } },
-        { src: hazelnut4, style: { bottom: "10%", right: "18%", width: "260px", rotate: "6deg" } },
+        {
+          src: hazelnut1,
+          style: { top: "10%", left: "20%", width: "180px", rotate: "-10deg" },
+        },
+        {
+          src: hazelnut2,
+          style: { top: "25%", right: "15%", width: "180px", rotate: "10deg" },
+        },
+        {
+          src: hazelnut3,
+          style: {
+            bottom: "12%",
+            left: "18%",
+            width: "180px",
+            rotate: "-8deg",
+          },
+        },
+        {
+          src: hazelnut4,
+          style: {
+            bottom: "10%",
+            right: "18%",
+            width: "180px",
+            rotate: "6deg",
+          },
+        },
       ],
     },
     {
       flavor: "Lychee Rose",
       title: "Ice Cream",
       desc: "A delicate fusion of lychee and rose, offering floral sweetness and exotic fruitiness in creamy perfection.",
-      colors: ["#ffe4e1", "#f9c0c0"],
+      colors: ["#eac7deff", "#f38bbaff"],
       main: lycheeroseIcecream,
+      mainStyle: { width: "730px"},
       fruits: [
-        { src: lychee1, style: { top: "10%", left: "12%", width: "260px", rotate: "-10deg" } },
-        { src: lychee2, style: { top: "25%", right: "15%", width: "260px", rotate: "10deg" } },
-        { src: rose1, style: { bottom: "12%", left: "18%", width: "260px", rotate: "-8deg" } },
-        { src: rose2, style: { bottom: "10%", right: "18%", width: "260px", rotate: "6deg" } },
+        {
+          src: lychee1,
+          style: { top: "15%", left: "12%", width: "300px", rotate: "-10deg" },
+        },
+        {
+          src: lychee2,
+          style: { top: "45%", right: "12%", width: "300px", rotate: "10deg" },
+        },
+        {
+          src: rose1,
+          style: {
+            bottom: "12%",
+            left: "25%",
+            width: "180px",
+            rotate: "-8deg",
+          },
+        },
+        {
+          src: rose2,
+          style: {
+            bottom: "60%",
+            right: "20%",
+            width: "150px",
+            rotate: "6deg",
+          },
+        },
       ],
     },
   ];
@@ -197,20 +400,45 @@ const Hero = () => {
 
   const imageVariants = {
     hidden: { opacity: 0, rotate: 0, scale: 0.95 },
-    visible: { opacity: 1, rotate: 100, scale: 1, transition: { duration: 0.8, ease: "easeOut" } },
-    exit: { opacity: 0, rotate: 0, transition: { duration: 0.4, ease: "easeIn" } },
+    visible: {
+      opacity: 1,
+      rotate: 100,
+      scale: 1,
+      transition: { duration: 0.8, ease: "easeOut" },
+    },
+    exit: {
+      opacity: 0,
+      rotate: 0,
+      transition: { duration: 0.4, ease: "easeIn" },
+    },
   };
 
   return (
-    <div className="hero" style={{ background: `radial-gradient(circle, ${c1} 0%, ${c1} 20%, ${c2} 20%, ${c2} 40%, ${c1} 40%, ${c1} 60%, ${c2} 60%, ${c2} 85%, ${c1} 85%, ${c1} 100%)` }}>
+    <div
+      className="hero"
+      style={{
+        background: `radial-gradient(circle, ${c1} 0%, ${c1} 20%, ${c2} 20%, ${c2} 40%, ${c1} 40%, ${c1} 60%, ${c2} 60%, ${c2} 85%, ${c1} 85%, ${c1} 100%)`,
+      }}
+    >
       <AnimatePresence mode="wait" custom={index}>
         <motion.div
           key={current.flavor}
           className="text-curve"
           variants={{
-            enter: (direction) => ({ y: direction === "next" ? 50 : -50, opacity: 0 }),
-            center: { y: 0, opacity: 1, transition: { duration: 0.6, ease: "easeOut" } },
-            exit: (direction) => ({ y: direction === "next" ? -50 : 50, opacity: 0, transition: { duration: 0.6, ease: "easeIn" } }),
+            enter: (direction) => ({
+              y: direction === "next" ? 50 : -50,
+              opacity: 0,
+            }),
+            center: {
+              y: 0,
+              opacity: 1,
+              transition: { duration: 0.6, ease: "easeOut" },
+            },
+            exit: (direction) => ({
+              y: direction === "next" ? -50 : 50,
+              opacity: 0,
+              transition: { duration: 0.6, ease: "easeIn" },
+            }),
           }}
           initial="enter"
           animate="center"
@@ -224,9 +452,29 @@ const Hero = () => {
       </AnimatePresence>
 
       <AnimatePresence mode="wait">
-        <motion.img key={current.main} src={current.main} alt={current.flavor} className="icecream" variants={imageVariants} initial="hidden" animate="visible" exit="exit" />
+        <motion.img
+          key={current.main}
+          src={current.main}
+          alt={current.flavor}
+          className="icecream"
+          style={current.mainStyle || {}}
+          variants={imageVariants}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+        />
         {current.fruits.map((fruit) => (
-          <motion.img key={fruit.src} src={fruit.src} alt="" className="fruit" style={fruit.style} variants={imageVariants} initial="hidden" animate="visible" exit="exit" />
+          <motion.img
+            key={fruit.src}
+            src={fruit.src}
+            alt=""
+            className="fruit"
+            style={fruit.style}
+            variants={imageVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+          />
         ))}
       </AnimatePresence>
 
